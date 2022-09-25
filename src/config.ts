dd362e8029c50800444806bce75738838c11b9d5
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-09-25 19:13:56 +0200

    INit blog

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Desvaríos de Pabs.';
+export const SITE_DESCRIPTION = 'Charlamos de todo y de nada. Programador, rolero y baloncestista no practicante.';
