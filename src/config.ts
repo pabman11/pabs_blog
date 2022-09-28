727402434c6bafcf84d6ab5a6bc0dfb644f5150a
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-09-28 22:09:05 +0200

    Refactor title on pages

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Desvaríos de Pabs.';
+export const SITE_TITLE = 'Los Delirios de Pabs.';
 export const SITE_DESCRIPTION = 'Charlamos de todo y de nada. Programador, rolero y baloncestista no practicante.';
-
-/* export const POSIBLE_TAGS = new Map([
-    ["dnd", {
-        "title": "Dungeons and Dragons"
-    }],
-    ["personal", {
-        "title": "Artículos personales"
-    }],
-    ["rol", {
-        "title": "Artículos sobre rol, en general"
-    }],
-    ["programacion", {
-        "title": "Artículos sobre programación web"
-    }],
-    ["relato", {
-        "title": "Relatos y literatura"
-    }],
-]) */
 
 export const POSIBLE_TAGS = [
     {
