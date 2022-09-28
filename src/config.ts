50847a4665fb6ae027db457c9ddf0b104e4eec45
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-09-28 20:21:43 +0200

    Fix tags and add a tags page

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,44 @@
 
 export const SITE_TITLE = 'Desvaríos de Pabs.';
 export const SITE_DESCRIPTION = 'Charlamos de todo y de nada. Programador, rolero y baloncestista no practicante.';
+
+/* export const POSIBLE_TAGS = new Map([
+    ["dnd", {
+        "title": "Dungeons and Dragons"
+    }],
+    ["personal", {
+        "title": "Artículos personales"
+    }],
+    ["rol", {
+        "title": "Artículos sobre rol, en general"
+    }],
+    ["programacion", {
+        "title": "Artículos sobre programación web"
+    }],
+    ["relato", {
+        "title": "Relatos y literatura"
+    }],
+]) */
+
+export const POSIBLE_TAGS = [
+    {
+        tag: "dnd",
+        title: "Dungeons and Dragons"
+    },
+    {
+        tag: "personal",
+        title: "Artículos personales"
+    },
+    {
+        tag: "rol",
+        title: "Artículos sobre rol, en general"
+    },
+    {
+        tag: "programacion",
+        title: "Artículos sobre programación web"
+    },
+    {
+        tag: "relato",
+        title: "Relatos y literatura"
+    },
+]
