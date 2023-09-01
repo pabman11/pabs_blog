bbb000b82a178a2c250aa1d2e5b58d1a87b83eb0
Pablo Fornali <pablofornalimanzanas@gmail.com>
2023-09-01 18:06:02 +0200

    Update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pabman11.github.io',
+  base: '/pabs_blog',
   markdown: {
-    drafts: true,
+    drafts: true
   },
-  integrations: [mdx(), sitemap(), tailwind(), ],
-});
+  integrations: [mdx(), sitemap(), tailwind()]
+})
