0e1cd7a3bf283670d23edb0f2e2486bfab310426
Pablo Fornali <pablofornalimanzanas@gmail.com>
2023-09-17 13:14:20 +0200

    Add DarkMode and Responsive menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
-				'sans': ['Outfit', ...defaultTheme.fontFamily.sans],
+				sans: ['Outfit', ...defaultTheme.fontFamily.sans]
 			}
-		},
+		}
 	},
-	plugins: [],
+	plugins: []
 }
