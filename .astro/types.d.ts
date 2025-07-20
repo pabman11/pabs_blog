5650e47391ea266b7ebc0bb931c870bf62a4f7ee
Pablo Fornali <pablofornalimanzanas@gmail.com>
2025-07-20 23:22:32 +0200

    Añadir contenido para "La Hoja en blanco" y actualizar tipos en el módulo de contenido

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -200,6 +200,13 @@ declare module 'astro:content' {
   collection: "blog";
   data: InferEntrySchema<"blog">
 } & { render(): Render[".mdx"] };
+"la-hoja-en-blanco.mdx": {
+	id: "la-hoja-en-blanco.mdx";
+  slug: "la-hoja-en-blanco";
+  body: string;
+  collection: "blog";
+  data: InferEntrySchema<"blog">
+} & { render(): Render[".mdx"] };
 };
 
 	};
